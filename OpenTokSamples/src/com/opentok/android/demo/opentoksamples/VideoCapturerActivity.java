22194be162234832bd8d6805d0fc7326968e92cd
Roberto Pérez Cubero <rpc@tid.es>
2014-08-06 13:20:12 +0200

    Compare using interface

--- OpenTokSamples/src/com/opentok/android/demo/opentoksamples/VideoCapturerActivity.java
+++ OpenTokSamples/src/com/opentok/android/demo/opentoksamples/VideoCapturerActivity.java
@@ -194,7 +194,7 @@ public class VideoCapturerActivity extends Activity implements
 
     private void unsubscribeFromStream(Stream stream) {
         mStreams.remove(stream);
-        if (mSubscriber.getStream().getStreamId().equals(stream.getStreamId())) {
+        if (mSubscriber.getStream().equals(stream)) {
             mSubscriberViewContainer.removeView(mSubscriber.getView());
             mSubscriber = null;
             if (!mStreams.isEmpty()) {
