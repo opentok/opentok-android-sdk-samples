9bbe44948f34686f03515f8a12223495b5abf4b4
marinas <marinas@tokbox.com>
2014-11-26 17:10:43 +0100

    Removing token and sessionId

--- OpenTokSamples/src/com/opentok/android/demo/config/OpenTokConfig.java
+++ OpenTokSamples/src/com/opentok/android/demo/config/OpenTokConfig.java
@@ -5,11 +5,11 @@ public class OpenTokConfig {
 	// *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
 	// ***                      https://dashboard.tokbox.com/projects                           ***
 	// Replace with a generated Session ID
-	public static final String SESSION_ID = "1_MX4xMDB-MTI3LjAuMC4xfjE0MTU5NTc3ODg3MjZ-UlBpcEUrSSt5TmE2V25hZGVRVWMvVUJafn4";
+	public static final String SESSION_ID = "";
 	// Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-	public static final String TOKEN = "T1==cGFydG5lcl9pZD0xMDAmc2RrX3ZlcnNpb249dGJwaHAtdjAuOTEuMjAxMS0wNy0wNSZzaWc9MGVkNzEwMmYyNjY5YTdkNGYyOTMyMzM5Nzk1ZTZhZWE3MGI4MGU5YjpzZXNzaW9uX2lkPTFfTVg0eE1EQi1NVEkzTGpBdU1DNHhmakUwTVRVNU5UYzNPRGczTWpaLVVsQnBjRVVyU1N0NVRtRTJWMjVoWkdWUlZXTXZWVUphZm40JmNyZWF0ZV90aW1lPTE0MTU5NTc3MTMmcm9sZT1tb2RlcmF0b3Imbm9uY2U9MTQxNTk1NzcxMy4xNDU4NDE3OTkyNDg5JmV4cGlyZV90aW1lPTE0MTg1NDk3MTM="; 
+	public static final String TOKEN = ""; 
 	// Replace with your OpenTok API key
-	public static final String API_KEY= "100";
+	public static final String API_KEY= "";
 	
 	// Subscribe to a stream published by this client. Set to false to subscribe
     // to other clients' streams only.
