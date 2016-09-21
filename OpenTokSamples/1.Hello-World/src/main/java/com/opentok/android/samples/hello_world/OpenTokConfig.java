c5c771ec56ccb59c1cacaea158e44fec38be37c1
Jose Antonio Olivera Ortega <josea.olivera@gmail.com>
2016-09-21 19:06:57 +0200

    OPENTOK-31056: New hello-world app module

---
+++ OpenTokSamples/1.Hello-World/src/main/java/com/opentok/android/samples/hello_world/OpenTokConfig.java
@@ -0,0 +1,17 @@
+package com.opentok.android.samples.hello_world;
+
+public class OpenTokConfig {
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "";
+    // Replace with a generated Session ID
+    public static final String SESSION_ID = "";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "";
+
+    // Subscribe to a stream published by this client. Set to false to subscribe
+    // to other clients' streams only.
+    public static final boolean SUBSCRIBE_TO_SELF = true;
+}
