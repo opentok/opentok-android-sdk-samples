dfcf4f9ea5ff5ce971ca6adddce8c8b67a65403d
marinas <marinaserranomontes@gmail.com>
2017-11-07 11:25:25 +0100

    Update sdk libs and applicationId

--- Basic-Video-Chat/app/src/main/java/com/tokbox/android/tutorials/basic_video_chat/OpenTokConfig.java
+++ Basic-Video-Chat/app/src/main/java/com/tokbox/android/tutorials/basic_video_chat/OpenTokConfig.java
@@ -7,11 +7,11 @@ public class OpenTokConfig {
     // ***                      https://dashboard.tokbox.com/projects                           ***
 
     // Replace with your OpenTok API key
-    public static final String API_KEY = "";
+    public static final String API_KEY = "100";
     // Replace with a generated Session ID
-    public static final String SESSION_ID = "";
+    public static final String SESSION_ID = "2_MX4xMDB-flR1ZSBOb3YgMTkgMTE6MDk6NTggUFNUIDIwMTN-MC4zNzQxNzIxNX4";
     // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-    public static final String TOKEN = "";
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD0xMDAmc2RrX3ZlcnNpb249dGJwaHAtdjAuOTEuMjAxMS0wNy0wNSZzaWc9Yjc0ZTBjZmQxMmJlY2Q5OTNiYmVhNzhiNzRkYjY5N2Y2YmIxNWY4ZDpzZXNzaW9uX2lkPTJfTVg0eE1EQi1mbFIxWlNCT2IzWWdNVGtnTVRFNk1EazZOVGdnVUZOVUlESXdNVE4tTUM0ek56UXhOekl4Tlg0JmNyZWF0ZV90aW1lPTE1MDc1NDMzOTEmcm9sZT1tb2RlcmF0b3Imbm9uY2U9MTUwNzU0MzM5MS40MDQ1MTY5OTE5OTc1NSZleHBpcmVfdGltZT0xNTEwMTM1Mzkx";
 
     /*                           ***** OPTIONAL *****
      If you have set up a server to provide session information replace the null value
