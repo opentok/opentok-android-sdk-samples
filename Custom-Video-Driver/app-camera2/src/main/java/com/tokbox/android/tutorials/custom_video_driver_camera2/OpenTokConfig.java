4f59981dd46a1ecb679f06c53a62543a4e8a5b08
Jint George <jintgeorge@gmail.com>
2020-03-16 14:07:44 -0700

    cleanup config file

--- Custom-Video-Driver/app-camera2/src/main/java/com/tokbox/android/tutorials/custom_video_driver_camera2/OpenTokConfig.java
+++ Custom-Video-Driver/app-camera2/src/main/java/com/tokbox/android/tutorials/custom_video_driver_camera2/OpenTokConfig.java
@@ -5,9 +5,9 @@ public class OpenTokConfig {
     // ***                      https://dashboard.tokbox.com/projects                           ***
 
     // Replace with your OpenTok API key
-    public static final String API_KEY = "46055102";
+    public static final String API_KEY = "";
     // Replace with a generated Session ID
-    public static final String SESSION_ID = "2_MX40NjA1NTEwMn5-MTU4NDEzODI1Njg3Nn4wVExrZ0V2RHF4ekVReW1SL3pVcXVYNUd-fg";
+    public static final String SESSION_ID = "";
     // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NjA1NTEwMiZzaWc9YzJhZWFiMzI3MTZmNzIzNTliNTY5OWJjNzkyN2VjNTg4ZTU4M2VkZDpzZXNzaW9uX2lkPTJfTVg0ME5qQTFOVEV3TW41LU1UVTROREV6T0RJMU5qZzNObjR3VkV4clowVjJSSEY0ZWtWUmVXMVNMM3BWY1hWWU5VZC1mZyZjcmVhdGVfdGltZT0xNTg0MTM4MjY2Jm5vbmNlPTAuMDY1MDAwNTAxNDQwMzA5MjImcm9sZT1tb2RlcmF0b3ImZXhwaXJlX3RpbWU9MTU4NDc0MzA2NSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==";
+    public static final String TOKEN = "";
 }
