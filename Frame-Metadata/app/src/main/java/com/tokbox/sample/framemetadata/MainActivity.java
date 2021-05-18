6311f8795855210954a62daf1d613f18097ee2a7
Igor <igor.wojda@gmail.com>
2021-05-18 20:31:49 +0200

    Update permissions

--- Frame-Metadata/app/src/main/java/com/tokbox/sample/framemetadata/MainActivity.java
+++ Frame-Metadata/app/src/main/java/com/tokbox/sample/framemetadata/MainActivity.java
@@ -218,12 +218,12 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
 
     @Override
     public void onPermissionsGranted(int requestCode, List<String> perms) {
-        Log.d(TAG, "onPermissionsGranted:" + requestCode + ":" + perms.size());
+        Log.d(TAG, "onPermissionsGranted:" + requestCode + ": " + perms);
     }
 
     @Override
     public void onPermissionsDenied(int requestCode, List<String> perms) {
-        finishWithMessage("onPermissionsDenied: " + requestCode + ":" + perms.size());
+        finishWithMessage("onPermissionsDenied: " + requestCode + ": " + perms);
     }
 
     @AfterPermissionGranted(PERMISSIONS_REQUEST_CODE)
