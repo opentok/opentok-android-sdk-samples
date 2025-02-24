c9979cf78604ec682e015c609d124662604ac4b8
Goncalo Mendes <goncalo.mendes@vonage.com>
2025-02-24 19:03:31 +0100

    Remove credentials

--- Basic-Video-Chat-With-ForegroundServices-Java/app/src/main/java/com/tokbox/sample/basicvideochatwithforegroundservices/OpenTokConfig.java
+++ Basic-Video-Chat-With-ForegroundServices-Java/app/src/main/java/com/tokbox/sample/basicvideochatwithforegroundservices/OpenTokConfig.java
@@ -14,13 +14,13 @@ public class OpenTokConfig {
     */
 
     // Replace with a API key
-    public static final String API_KEY = "47521351";
+    public static final String API_KEY = "";
     
     // Replace with a generated Session ID
-    public static final String SESSION_ID = "1_MX40NzUyMTM1MX5-MTczOTM1NTY1Njk5N35FS3RnTm5yTlJqMnBMbjQ0Z0hvSVNhcXZ-fn4";
+    public static final String SESSION_ID = "";
     
     // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NzUyMTM1MSZzaWc9MTEwNTUzYTEyZWU3ZmNlMTQwODYwMDNjNjNiY2MyNjliMjM3OTNlOTpzZXNzaW9uX2lkPTFfTVg0ME56VXlNVE0xTVg1LU1UY3pPVE0xTlRZMU5qazVOMzVGUzNSblRtNXlUbEpxTW5CTWJqUTBaMGh2U1ZOaGNYWi1mbjQmY3JlYXRlX3RpbWU9MTczOTM1NTY2MiZub25jZT0wLjk1MDM2MTM5Njg3MDc2MjMmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTc0MTk0NzY2MSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==";
+    public static final String TOKEN = "";
 
     // *** The code below is to validate this configuration file. You do not need to modify it  ***
 
