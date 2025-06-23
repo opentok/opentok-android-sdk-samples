5dd80bc4de17f974d539f00a2d135c4f009f0e76
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-06-23 14:32:14 +0200

    Removed unused request audio focus

--- Basic-Video-Chat-ConnectionService/app/src/main/java/com/tokbox/sample/basicvideochat_connectionservice/VonageManager.java
+++ Basic-Video-Chat-ConnectionService/app/src/main/java/com/tokbox/sample/basicvideochat_connectionservice/VonageManager.java
@@ -1,11 +1,7 @@
 package com.tokbox.sample.basicvideochat_connectionservice;
 
 import android.content.Context;
-import android.media.AudioAttributes;
-import android.media.AudioFocusRequest;
-import android.media.AudioManager;
 import android.opengl.GLSurfaceView;
-import android.os.Build;
 import android.util.Log;
 
 import com.opentok.android.AudioDeviceManager;
@@ -29,11 +25,8 @@ public class VonageManager {
     private Subscriber subscriber;
     private Context context;
     private final VonageSessionListener callback;
-    private AudioManager audioManager;
     private AudioDeviceManager audioDeviceManager;
     private BaseAudioDevice.AudioFocusManager audioFocusManager;
-
-    private boolean audioFocusActive = false;
 
     private static VonageManager instance;
 
@@ -218,86 +211,6 @@ public class VonageManager {
         }
         audioFocusManager.audioFocusDeactivated();
     }
-
-    public boolean requestAudioFocus(Context context) {
-        audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-
-        if (audioManager != null && !audioFocusActive) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                AudioFocusRequest audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
-                        .setAudioAttributes(new AudioAttributes.Builder()
-                                .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
-                                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                                .build())
-                        .setOnAudioFocusChangeListener(audioFocusChangeListener)
-                        .build();
-
-                int result = audioManager.requestAudioFocus(audioFocusRequest);
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    Log.d("VonageCallManager", "Audio focus granted");
-                    audioFocusActive = true;
-                } else if (result == AudioManager.AUDIOFOCUS_REQUEST_DELAYED) {
-                    Log.d("VonageCallManager", "Audio focus delayed");
-                } else {
-                    Log.e("VonageCallManager", "Failed to gain audio focus");
-                }
-            } else {
-                int result = audioManager.requestAudioFocus(
-                        audioFocusChangeListener,
-                        AudioManager.STREAM_VOICE_CALL,
-                        AudioManager.AUDIOFOCUS_GAIN
-                );
-
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    Log.d("VonageConnection", "Audio focus granted");
-                    audioFocusActive = true;
-                } else if (result == AudioManager.AUDIOFOCUS_REQUEST_DELAYED) {
-                    Log.d("VonageCallManager", "Audio focus delayed");
-                } else {
-                    Log.e("VonageConnection", "Failed to gain audio focus");
-                }
-            }
-        }
-        return audioFocusActive;
-    }
-
-    public void releaseAudioFocus() {
-        if (audioManager != null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                AudioFocusRequest focusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
-                        .setOnAudioFocusChangeListener(audioFocusChangeListener)
-                        .build();
-                audioManager.abandonAudioFocusRequest(focusRequest);
-            } else {
-                audioManager.abandonAudioFocus(audioFocusChangeListener);
-            }
-            audioFocusActive = false;
-            Log.d("VonageConnection", "Audio focus released");
-        }
-    }
-
-    private final AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = focusChange -> {
-        switch (focusChange) {
-            case AudioManager.AUDIOFOCUS_GAIN:
-            case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT:
-            case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK:
-            case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE:
-                Log.d("VonageConnection", "Audio focus gained");
-                notifyAudioFocusIsActive();
-                break;
-
-            case AudioManager.AUDIOFOCUS_LOSS:
-            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-                Log.d("VonageConnection", "Audio focus lost");
-                notifyAudioFocusIsInactive();
-                break;
-
-            default:
-                Log.d("VonageConnection", "Unknown audio focus state: " + focusChange);
-                break;
-        }
-    };
 
     public void endCall() {
         VonageConnection connection = VonageConnectionHolder.getInstance().getConnection();
