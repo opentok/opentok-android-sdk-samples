97e9b3ef828c15fedf23990a071918381602b21f
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-06-30 14:39:46 +0200

    Removed proguard

--- Basic-Video-Chat-ConnectionService-Kotlin/app/build.gradle.kts
+++ Basic-Video-Chat-ConnectionService-Kotlin/app/build.gradle.kts
@@ -23,10 +23,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
