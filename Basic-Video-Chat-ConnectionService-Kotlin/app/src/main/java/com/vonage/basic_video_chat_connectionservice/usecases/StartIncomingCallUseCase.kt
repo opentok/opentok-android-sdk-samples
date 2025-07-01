a21bb7381699ff9e8a58186a1296195eb840ddb4
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-07-01 14:22:26 +0200

    Credential checks before notifiying calls to the telecomManager

--- Basic-Video-Chat-ConnectionService-Kotlin/app/src/main/java/com/vonage/basic_video_chat_connectionservice/usecases/StartIncomingCallUseCase.kt
+++ Basic-Video-Chat-ConnectionService-Kotlin/app/src/main/java/com/vonage/basic_video_chat_connectionservice/usecases/StartIncomingCallUseCase.kt
@@ -1,6 +1,7 @@
 package com.vonage.basic_video_chat_connectionservice.usecases
 
 import com.vonage.basic_video_chat_connectionservice.CallException
+import com.vonage.basic_video_chat_connectionservice.OpenTokConfig
 import com.vonage.basic_video_chat_connectionservice.connectionservice.PhoneAccountManager
 import javax.inject.Inject
 
@@ -12,10 +13,14 @@ private val phoneAccountManager: PhoneAccountManager
         val callerName = "Simulated Caller"
         val callerId = "+4401539702257"
 
-        if (phoneAccountManager.canPlaceIncomingCall()) {
-            phoneAccountManager.notifyIncomingVideoCall(callerName, callerId)
-        } else {
+        if (!OpenTokConfig.isValid) {
+            throw CallException("Invalid credentials", 1001)
+        }
+
+        if (!phoneAccountManager.canPlaceIncomingCall()) {
             throw CallException("Can't launch incoming call", 1001)
         }
+
+        phoneAccountManager.notifyIncomingVideoCall(callerName, callerId)
     }
 }
--- Basic-Video-Chat-ConnectionService-Kotlin/app/src/main/java/com/vonage/basic_video_chat_connectionservice/usecases/StartOutgoingCallUseCase.kt
+++ Basic-Video-Chat-ConnectionService-Kotlin/app/src/main/java/com/vonage/basic_video_chat_connectionservice/usecases/StartOutgoingCallUseCase.kt
@@ -1,6 +1,7 @@
 package com.vonage.basic_video_chat_connectionservice.usecases
 
 import com.vonage.basic_video_chat_connectionservice.CallException
+import com.vonage.basic_video_chat_connectionservice.OpenTokConfig
 import com.vonage.basic_video_chat_connectionservice.connectionservice.PhoneAccountManager
 import javax.inject.Inject
 
@@ -12,10 +13,14 @@ private val phoneAccountManager: PhoneAccountManager
         val callerName = "Simulated Caller"
         val callerId = "+4401539702257"
 
-        if (phoneAccountManager.canPlaceOutgoingCall()) {
-            phoneAccountManager.startOutgoingVideoCall(callerName, callerId)
-        } else {
+        if (!OpenTokConfig.isValid) {
+            throw CallException("Invalid credentials", 1001)
+        }
+
+        if (!phoneAccountManager.canPlaceOutgoingCall()) {
             throw CallException("Can't launch outgoing call", 1001)
         }
+
+        phoneAccountManager.startOutgoingVideoCall(callerName, callerId)
     }
 }
