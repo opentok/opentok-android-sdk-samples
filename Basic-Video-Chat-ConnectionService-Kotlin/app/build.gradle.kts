2521c72a15197e500cf06ffd303ae630709c0598
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-11-03 22:23:22 +0100

    Referencing the latest OpenTok version from the toml file

--- Basic-Video-Chat-ConnectionService-Kotlin/app/build.gradle.kts
+++ Basic-Video-Chat-ConnectionService-Kotlin/app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.opentok.android.sdk)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
