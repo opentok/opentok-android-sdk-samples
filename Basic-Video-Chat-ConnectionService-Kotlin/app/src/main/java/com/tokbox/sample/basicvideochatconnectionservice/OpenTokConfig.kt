828ebd9a062242b7a149c998edf0b8b8b054630a
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-11-05 14:54:38 +0100

    Added empty Readme Removed debug room keys from config file Added workflow

--- Basic-Video-Chat-ConnectionService-Kotlin/app/src/main/java/com/tokbox/sample/basicvideochatconnectionservice/OpenTokConfig.kt
+++ Basic-Video-Chat-ConnectionService-Kotlin/app/src/main/java/com/tokbox/sample/basicvideochatconnectionservice/OpenTokConfig.kt
@@ -11,19 +11,13 @@ object OpenTokConfig {
     valid URL.
     */
     // Replace with a API key
-    const val API_KEY: String = "1cc1c8de-7e50-497b-b0b1-7d8e1ae46d38"
-
- //   {
- //       "sessionId": "2_MX4xY2MxYzhkZS03ZTUwLTQ5N2ItYjBiMS03ZDhlMWFlNDZkMzh-fjE3NjIzNDk5NzY0ODF-ZXFIa3pQZ2l5SUxCeC84QStnWHJDK3Zkfn5-",
- //       "token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InNlc3Npb24uY29ubmVjdCIsInNlc3Npb25faWQiOiIyX01YNHhZMk14WXpoa1pTMDNaVFV3TFRRNU4ySXRZakJpTVMwM1pEaGxNV0ZsTkRaa016aC1makUzTmpJek5EazVOelkwT0RGLVpYRklhM3BRWjJsNVNVeENlQzg0UVN0bldISkRLM1prZm41LSIsInJvbGUiOiJtb2RlcmF0b3IiLCJpbml0aWFsX2xheW91dF9jbGFzc19saXN0IjoiIiwiZXhwIjoxNzYyNDM2Mzc3LCJzdWIiOiJ2aWRlbyIsImFjbCI6eyJwYXRocyI6eyIvc2Vzc2lvbi8qKiI6e319fSwianRpIjoiOTAwMGFlNmQtMDJlYy00NTU0LTljMjItZDIzNDg1NjgxYjc5IiwiaWF0IjoxNzYyMzQ5OTc2LCJhcHBsaWNhdGlvbl9pZCI6IjFjYzFjOGRlLTdlNTAtNDk3Yi1iMGIxLTdkOGUxYWU0NmQzOCJ9.P01ISnKxpAgJc8M-8KKcYYKVLQZ7mP1Hl74xN3kOk9yJipd6ygXe9flIZ3gs3mFK1dnVdNAFyf9SlkCqKBoATEAXXH6tRuEe5gw5821LHTvmsjRXjC6sXvm21pfgNrgPI_oUPikIeaadxDUsk9TTdAwx7k9-g0BeEe5bHTKDNdH47FzSfm5shesayKofx9H9PTv8AES9YkL21P-B3DYnAypt2bnIrjgtdAzVZ2B4nWuFGW7DdZbgPR8rUfC16rl5XThlZJMsaUM2PL8h1JKonYQaND67RUEZQiw9sXDEZzPDkCE6cq3guwc81SvLuRAfZEUZ2GHwIgfKBS-igJ5ftw",
- //       "apiKey": "1cc1c8de-7e50-497b-b0b1-7d8e1ae46d38",
- //       "captionsId": null
+    const val API_KEY: String = ""
 
     // Replace with a generated Session ID
-    const val SESSION_ID: String = "2_MX4xY2MxYzhkZS03ZTUwLTQ5N2ItYjBiMS03ZDhlMWFlNDZkMzh-fjE3NjIzNDk5NzY0ODF-ZXFIa3pQZ2l5SUxCeC84QStnWHJDK3Zkfn5-"
+    const val SESSION_ID: String = ""
 
     // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-    const val TOKEN: String = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InNlc3Npb24uY29ubmVjdCIsInNlc3Npb25faWQiOiIyX01YNHhZMk14WXpoa1pTMDNaVFV3TFRRNU4ySXRZakJpTVMwM1pEaGxNV0ZsTkRaa016aC1makUzTmpJek5EazVOelkwT0RGLVpYRklhM3BRWjJsNVNVeENlQzg0UVN0bldISkRLM1prZm41LSIsInJvbGUiOiJtb2RlcmF0b3IiLCJpbml0aWFsX2xheW91dF9jbGFzc19saXN0IjoiIiwiZXhwIjoxNzYyNDM2Mzc3LCJzdWIiOiJ2aWRlbyIsImFjbCI6eyJwYXRocyI6eyIvc2Vzc2lvbi8qKiI6e319fSwianRpIjoiOTAwMGFlNmQtMDJlYy00NTU0LTljMjItZDIzNDg1NjgxYjc5IiwiaWF0IjoxNzYyMzQ5OTc2LCJhcHBsaWNhdGlvbl9pZCI6IjFjYzFjOGRlLTdlNTAtNDk3Yi1iMGIxLTdkOGUxYWU0NmQzOCJ9.P01ISnKxpAgJc8M-8KKcYYKVLQZ7mP1Hl74xN3kOk9yJipd6ygXe9flIZ3gs3mFK1dnVdNAFyf9SlkCqKBoATEAXXH6tRuEe5gw5821LHTvmsjRXjC6sXvm21pfgNrgPI_oUPikIeaadxDUsk9TTdAwx7k9-g0BeEe5bHTKDNdH47FzSfm5shesayKofx9H9PTv8AES9YkL21P-B3DYnAypt2bnIrjgtdAzVZ2B4nWuFGW7DdZbgPR8rUfC16rl5XThlZJMsaUM2PL8h1JKonYQaND67RUEZQiw9sXDEZzPDkCE6cq3guwc81SvLuRAfZEUZ2GHwIgfKBS-igJ5ftw"
+    const val TOKEN: String = ""
 
     val isValid: Boolean
         // *** The code below is to validate this configuration file. You do not need to modify it  ***
